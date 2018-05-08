4807714fa774597b372c72051ca90e3a298ce147
PitKwang <PitKwang@gmail.com>
2018-05-08 15:14:03 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -2,7 +2,10 @@ package oopmit;
 
 public class Hello {
     public static void main(String[] args) {
-        
+
         System.out.println("Hello, My name is Pitchayakan");
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV")
     }//main
 }//class
